f3dd60c0cf3cc99208acf0a32cf45d2880379a7d
Nits László <nits@jedlik.helyi>
2017-09-20 09:21:41 +0200

    Upload

--- ts/content.ts
+++ ts/content.ts
@@ -6,7 +6,7 @@ export class Content {
 
     Content(req: http.ServerRequest, res: http.ServerResponse): void {
         res.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
-        res.write("<p>Hello TypeScript!</p>");
+        res.write("<p>Hello TypeScript!!!</p>");
 
 
         res.end();
